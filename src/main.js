b709d7f2d7ec8714842a9e1c2064c672bd13fd3c
zhangyuchi <2669327287@qq.com>
2020-02-19 13:31:49 +0800

    生成自定义css文件

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store'
 /*配置element-ui  如果后期需要减小体积可以将这块内容移至js文件中*/
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
+import '../theme/index.css'
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
