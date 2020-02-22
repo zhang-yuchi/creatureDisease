1a9ba7664659be172cc2fbdbdaef24bc52472c66
zhangyuchi <2669327287@qq.com>
2020-02-22 10:34:19 +0800

    引入订单详情组件

--- src/components/order-detail/check-control.vue
+++ src/components/order-detail/check-control.vue
@@ -6,10 +6,12 @@
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-
+import Ititle from '../title/index'
 export default {
 //import引入的组件需要注入到对象中才能使用
-components: {},
+components: {
+    Ititle,
+},
 data() {
 //这里存放数据
 return {
--- src/views/manager/orderDetail.vue
+++ src/views/manager/orderDetail.vue
@@ -1,15 +1,48 @@
 <!--  -->
 <template>
-<div class=''></div>
+<div class=''>
+    <!-- 检测中按钮 -->
+    <check-control></check-control>
+    <!-- 订单信息 -->
+    <order-detail></order-detail>
+    <!-- 订单进度 -->
+    <order-process></order-process>
+    <!-- 订单明细 -->
+    <order-line></order-line>
+    <!-- 送检信息 -->
+    <transport-info></transport-info>
+    <!-- 样本信息 -->
+    <sample-info></sample-info>
+    <!-- 检测报告 -->
+    <report></report>
+    <!-- 物流信息 -->
+    <logistics></logistics>
+</div>
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-
+import checkControl from '../../components/order-detail/check-control'
+import orderDetail from '../../components/order-detail/order-detail'
+import orderProcess from '../../components/order-detail/order-process'
+import orderLine from '../../components/order-detail/order-line'
+import transportInfo from '../../components/order-detail/transport-info'
+import sampleInfo from '../../components/order-detail/sample-info'
+import report from '../../components/order-detail/report'
+import logistics from '../../components/order-detail/logistics'
 export default {
 //import引入的组件需要注入到对象中才能使用
-components: {},
+components: {
+    checkControl,
+    orderDetail,
+    orderProcess,
+    orderLine,
+    transportInfo,
+    sampleInfo,
+    report,
+    logistics,
+},
 data() {
 //这里存放数据
 return {
